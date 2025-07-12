1b862a6a409a6731636be62dc8b3f65cbddb50da
younghoo <free_learner@163.com>
2025-07-12 19:36:50 +0800

    echo a line to separate texts

---
+++ code/ZOTHERS/make_segline.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+## Author: Yang Hu / free_learner@163.com
+
+## This script makes a line of characters to segment text into blocks
+
+## Function to make the segment line
+make_segline() {
+  echo $(printf '%.s'$1 $(seq 1 $2))
+}
+
+## Make the line
+CHAR='-'
+NC=50
+make_segline $CHAR $NC
+
+
