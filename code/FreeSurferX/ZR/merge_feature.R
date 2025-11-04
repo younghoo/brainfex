a2a0692a693f807d2ea8e73e4a688d4e6e1305a7
Alex <free_learner@163.com>
2025-11-04 21:24:32 +0800

    Extract cortical features based on parcellation atlases

---
+++ code/FreeSurferX/ZR/merge_feature.R
@@ -0,0 +1,36 @@
+library(stringr)
+## Merge lh and rh features, and remove interesting data
+## Get the commandline arguments
+cmd_args <- commandArgs(trailingOnly = TRUE)
+lh_fname <- cmd_args[1]
+rh_fname <- cmd_args[2]
+out_fname <- cmd_args[3]
+## Loop each hemisphere
+all_dat <- NULL
+for (curr_hemi in c('lh', 'rh')){
+  if (curr_hemi == 'lh'){
+    curr_dat <- read.table(lh_fname, header = TRUE, check.names = FALSE)
+  }else{
+    curr_dat <- read.table(rh_fname, header = TRUE, check.names = FALSE)
+  }
+  ## Remove the first column, representing the SUBJECT ID
+  curr_dat <- curr_dat[,-1]
+  ## Remove uninteresting data
+  rm_idx <- names(curr_dat) %in% c("lh_MeanThickness_thickness", "rh_MeanThickness_thickness",
+                                    "lh_WhiteSurfArea_area", "rh_WhiteSurfArea_area",
+                                    "BrainSegVolNotVent", "eTIV")
+  curr_dat <- curr_dat[, !rm_idx]
+  ## Combine the lh and rh data
+  if (is.null(all_dat)){
+    all_dat <- curr_dat
+  }else{
+    all_dat <- cbind(all_dat, curr_dat)
+  }
+}
+## Refine column names
+names(all_dat) <- stringr::str_remove(names(all_dat), '_(thickness|area|volume|meancurv|gauscurv|curvind|foldind)')
+names(all_dat) <- stringr::str_replace_all(names(all_dat), fixed('&'), '_and_')
+names(all_dat) <- stringr::str_replace_all(names(all_dat), fixed('-'), '_')
+## Save
+write.table(all_dat, out_fname, quote=FALSE, row.names=FALSE, col.names=TRUE)
+
---
+++ code/FreeSurferX/extract_cortical_feature.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+
+## Author: Yang Hu / free_learner@163.com
+
+## Make the segment line
+if [[ ! -z ${BrainFex} ]]
+then
+    segline=$(bash ${BrainFex}/code/ZOTHERS/make_segline.sh)
+fi
+
+## Print script usage
+Usage () {
+cat <<USAGE
+$segline
+This script extracts cortical features from recon-all output and a parcellation atlas
+$segline
+Usage example:
+bash $0 
+        -a /home/alex/reconout
+        -b Schaefer
+        -c thickness
+        -d /home/alex/output
+        -e Schaefer_ThickAvg.txt
+$segline
+Required arguments:
+        -a: recon-all output folder
+        -b: parcellation atlas name
+        -c: cortical feature name
+        -d: output folder
+        -e: output filename
+$segline
+Parcellation atlases available: 
+        1. DK
+        2. Destrieux
+$segline
+Cortical features available: 
+        1. thickness
+        2. area
+        3. volume
+        4. meancurv
+        5. gauscurv
+        6. foldind
+        7. curvind
+$segline
+USAGE
+    exit 1
+}
+
+## Parse arguments
+if [[ $# -lt 10 ]]
+then
+    Usage >&2
+    exit 1
+else
+    while getopts "a:b:c:d:e:" OPT
+    do
+      case $OPT in
+            a) ## recon-all output folder
+                RECONOUT=$OPTARG
+            ;;
+            b) ## parcellation atlas name
+                PARCNAME=$OPTARG
+            ;;
+            c) ## cortical feature name
+                MEASNAME=$OPTARG
+            ;;
+            d) ## output folder
+                OUTDIR=$OPTARG
+            ;;
+            e) ## output filename
+                OUTFILE=$OPTARG
+            ;;
+            *) ## invalid option
+                echo "ERROR: Unrecognized option -$OPT $OPTARG. Please check !!!"
+                exit 1
+            ;;
+      esac
+    done
+fi
+
+## If BrainFex environment variable set?
+if [[ -z ${BrainFex} ]]
+then
+    echo "ERROR: Please set the BrainFex environment variable !!!"
+    exit 1
+fi
+
+## If INPUT/OUTPUT folders exist?
+bash ${BrainFex}/code/ZOTHERS/check_inout.sh -b ${RECONOUT} -b ${OUTDIR}
+if [[ $? -eq 1 ]]
+then
+    exit 1
+fi
+
+## Deal with atlas names of default parcellations in FreeSurfer
+## Reference: https://surfer.nmr.mgh.harvard.edu/fswiki/CorticalParcellation
+## In recon-all, "aparc" stands for the DK Atlas.
+if [[ ${PARCNAME} == 'DK' ]]
+then
+    PARCNAME=aparc
+fi
+## In recon-all, "aparc.a2009s" stands for the Destrieux Atlas.
+if [[ ${PARCNAME} == 'Destrieux' ]]
+then
+    PARCNAME=aparc.a2009s
+fi
+
+## Set FreeSurfer environment variables
+export SUBJECTS_DIR=$(dirname ${RECONOUT})
+SUBJECT=$(basename ${RECONOUT})
+
+## Extract data for each hemisphere
+for curr_hemi in lh rh
+do
+    aparcstats2table --subjects ${SUBJECT} --hemi ${curr_hemi} --parc ${PARCNAME} --meas ${MEASNAME} \
+        --tablefile ${OUTDIR}/${curr_hemi}_${PARCNAME}_${MEASNAME}.txt
+done
+
+## Combine lh and rh features, and remove uninteresting data
+Rscript ${BrainFex}/code/FreeSurferX/ZR/merge_feature.R ${OUTDIR}/lh_${PARCNAME}_${MEASNAME}.txt ${OUTDIR}/rh_${PARCNAME}_${MEASNAME}.txt ${OUTDIR}/${OUTFILE}
+
+## Remove intermediate files
+rm ${OUTDIR}/?h_${PARCNAME}_${MEASNAME}.txt
+
+## Check the success of feature extraction
+if [[ ! -f ${OUTDIR}/${OUTFILE} ]]
+then
+    echo "ERROR: the extraction of cortical feature failed. Please check !!!"
+    exit 1
+fi
