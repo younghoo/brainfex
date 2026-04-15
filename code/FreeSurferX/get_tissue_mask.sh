c63957bcae06cf1aa6046ad80c0547a6c9d362ae
Alex <free_learner@163.com>
2026-04-15 10:47:06 +0800

    Remove the output prefix option in get_tissue_mask.sh

--- code/FreeSurferX/get_tissue_mask.sh
+++ code/FreeSurferX/get_tissue_mask.sh
@@ -18,24 +18,22 @@ Usage example:
 bash $0 
         -a /home/alex/reconout
         -b /home/alex/output
-        -c t1
 $segline
 Required arguments:
         -a: recon-all output folder
         -b: output folder to save masks
-        -c: output prefix
 $segline
 USAGE
     exit 1
 }
 
 ## Parse arguments
-if [[ $# -lt 6 ]]
+if [[ $# -lt 4 ]]
 then
     Usage >&2
     exit 1
 else
-    while getopts "a:b:c:" OPT
+    while getopts "a:b:" OPT
     do
         case $OPT in
             a) ## recon-all output folder
@@ -43,9 +41,6 @@ else
                 ;;
             b) ## output folder
                 OUTDIR=$OPTARG
-                ;;
-            c) ## output prefix
-                OUTFIX=$OPTARG
                 ;;
             *) ## invalid option
                 echo "ERROR: Unrecognized option -$OPT $OPTARG. Please check !!!"
@@ -70,19 +65,19 @@ then
 fi
 
 ## Extract brain mask
-mri_convert ${RECONOUT}/mri/nu.mgz ${OUTDIR}/${OUTFIX}_bfc.nii.gz
-mri_binarize --i ${RECONOUT}/mri/aseg.mgz --min 1 --o ${OUTDIR}/${OUTFIX}_brainmask.nii.gz
-mri_mask ${RECONOUT}/mri/nu.mgz ${OUTDIR}/${OUTFIX}_brainmask.nii.gz ${OUTDIR}/${OUTFIX}_brain.nii.gz
+mri_convert ${RECONOUT}/mri/nu.mgz ${OUTDIR}/bfc.nii.gz
+mri_binarize --i ${RECONOUT}/mri/aseg.mgz --min 1 --o ${OUTDIR}/brain_mask.nii.gz
+mri_mask ${RECONOUT}/mri/nu.mgz ${OUTDIR}/brain_mask.nii.gz ${OUTDIR}/brain.nii.gz
 
 ## Extract GM/WM/ventricle masks
 ## Note the ventricle mask including Choroid Plexus but not Fourth Ventricle
-mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/${OUTFIX}_wmmask.nii.gz --all-wm
-mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/${OUTFIX}_gmmask.nii.gz --gm
-mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/${OUTFIX}_ventmask.nii.gz --ventricles
+mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/wm_mask.nii.gz --all-wm
+mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/gm_mask.nii.gz --gm
+mri_binarize --i ${RECONOUT}/mri/aseg.mgz --o ${OUTDIR}/vent_mask.nii.gz --ventricles
 
 ## Check whether the output files exist
-bash ${BrainFex}/code/ZOTHERS/check_inout.sh -a ${OUTDIR}/${OUTFIX}_bfc.nii.gz -a ${OUTDIR}/${OUTFIX}_brainmask.nii.gz \
-    -a ${OUTDIR}/${OUTFIX}_gmmask.nii.gz -a ${OUTDIR}/${OUTFIX}_wmmask.nii.gz -a ${OUTDIR}/${OUTFIX}_ventmask.nii.gz
+bash ${BrainFex}/code/ZOTHERS/check_inout.sh -a ${OUTDIR}/bfc.nii.gz -a ${OUTDIR}/brain_mask.nii.gz \
+    -a ${OUTDIR}/gm_mask.nii.gz -a ${OUTDIR}/wm_mask.nii.gz -a ${OUTDIR}/vent_mask.nii.gz
 if [[ $? -eq 1 ]]
 then
     echo "ERROR: The extraction of tissue masks failed. Please check !!!"
